33ff0060bf213755d096c0c8b5548e4746ad2dcb
Jefferson Aggor <aggorjefferson@gmail.com>
2021-02-07 21:02:18 +0000

    helmet and hpp removed

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const dotenv = require('dotenv');
-const express = require('express');
-const hpp = require('hpp');
-const helmet = require('helmet');
-const cors = require('cors');
-const cookieParser = require('cookie-parser');
+const dotenv = require("dotenv");
+const express = require("express");
+// const hpp = require('hpp');
+// const helmet = require('helmet');
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
@@ -12,29 +12,28 @@ dotenv.config();
 //Middlewares
 app.use(cookieParser());
 
-app.use(cors())
-app.use(helmet());
-app.use(hpp())
+app.use(cors());
+// app.use(helmet());
+// app.use(hpp())
 // Files
-const {connectDB} = require('./config/db');
+const { connectDB } = require("./config/db");
 
 // Router files
-const index = require('./routes/index');
-const workers = require("./routes/workers")
+const index = require("./routes/index");
+const workers = require("./routes/workers");
 
 // parse application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: false }))
- 
+app.use(express.urlencoded({ extended: false }));
+
 // parse application/json
-app.use(express.json())
+app.use(express.json());
 
 // Database connection;
-connectDB()
+connectDB();
 
 const PORT = process.env.PORT || 5000;
 
+app.use("/", index);
+app.use("/api/workers", workers);
 
-app.use('/',index);
-app.use('/api/workers',workers);
-
-app.listen(PORT,()=> console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
